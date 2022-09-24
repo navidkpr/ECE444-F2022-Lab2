a9ab0dca0e9132aac1a70ad2ec139bc34e051b4c
navidkpr <navid.pour@outlook.com>
2022-09-24 11:06:44 -0400

    add templates

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return "<h1>Hello, {}!</h1>".format(name)
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     load_dotenv()
